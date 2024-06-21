810325d949398811171d2cb930f5c79ef3f6f28b
Danyal Ali Asghar <danyal.aliasghar@gmail.com>
2024-06-21 20:23:32 +0500

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -1,7 +1,7 @@
 import torch
 
 def perform_inference(model, sentence, word_to_index, tag_to_index, device):
-    sentence_tensor = torch.tensor([word_to_index.get(word, word_to_index["<unk>"]) for word in sentence.split(" ")]).to(device)
+    sentence_tensor = torch.tensor([word_to_index.get(word, word_to_index["<unk>"]) for word in sentence.split()]).to(device)
     model.eval()
     with torch.no_grad():
         output = model(sentence_tensor)
